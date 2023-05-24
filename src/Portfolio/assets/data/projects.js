bb459f89c5188925329b8ebd971a3ac178883b09
Tchinonso <lightersmercury@gmail.com>
2023-05-24 09:38:56 +0100

    project images updated

--- src/Portfolio/assets/data/projects.js
+++ src/Portfolio/assets/data/projects.js
@@ -19,7 +19,7 @@ const projects = [
     id: uuidv4(),
     name: 'BetMart',
     desc:
-      'A pure betting site, just the likes of Betkings, You can place a bet between two teams, in football,tennis,basket ball and others.',
+      'A pure betting site, just like the likes of Betkings and bet naija, You can place a bet between two teams, in football,tennis,basket ball and others.',
     img: BetMart,
     link: 'https://tchinonso.github.io/betmart'
   },
@@ -27,7 +27,7 @@ const projects = [
     id: uuidv4(),
     name: 'AdamSonic',
     desc:
-      'This website is a portfolio website for a medical and pharmaceutical firm, you can other order their product and contact their staff',
+      'This website is a portfolio website for a medical and pharmaceutical firm, you can order their products and contact their staff',
     img: Adams,
     link: 'https://tchinonso.github.io/adams'
   },
@@ -35,17 +35,9 @@ const projects = [
     id: uuidv4(),
     name: "PIFORTUNE MARKET",
     desc:
-      'A business crypto website, where customers can trade their pi coin and also purchase a product.',
+      'A business crypto website, where customers can trade their pi coin and also purchase products and pay with pi coin.',
     img: Pifortune,
     link: 'https://pifortunemarket.site'
-  },
-  {
-    id: uuidv4(),
-    name: 'DCC',
-    desc:
-      'This project is yet to be completed, it is done halfway, only functional on mobile screens, it is also crypto related.',
-    img: DCC,
-    link: 'https://tchinonso.github.io/dcc'
   },
   {
     id: uuidv4(),
@@ -54,6 +46,14 @@ const projects = [
       'A business website where you can trade your pi coin and also purchase goods and services and pay with pi.',
     img: PimallMetro,
     link: 'https://pimallmetro.com'
+  },
+  {
+    id: uuidv4(),
+    name: 'DCC',
+    desc:
+      'This project is yet to be completed, it is done halfway, only functional on mobile screens, it is also crypto related.',
+    img: DCC,
+    link: 'https://tchinonso.github.io/dcc'
   },
 ];
 
